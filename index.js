72a85f0bb62a65ece8bca61f123877b07c2b482e
Mandy Strickland <mandy.strickland92@gmail.com>
2019-03-10 16:39:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ function logWhisper(string){
   console.log(string.toLowerCase())
 }
 
+<<<<<<< HEAD
 function sayHiToGrandma(string){
   console.log (string.toUpperCase())
   return "YES INDEED"
@@ -34,4 +35,17 @@ function sayHiToGrandma(string){
 else if(string === "I love you, Grandma."){
   return "I love you, too."
 }
+=======
+
+function sayHiToGrandma(string){
+  console.log(string.toLowerCase()) 
+  return "I can't hear you!" }
+
+
+function sayHiToGrandma(string){
+  if(sayHiToGrandma.toUpperCase() === sayHiToGrandma){
+    return "YES INDEED!"}
+    if(sayHiToGrandma.toLowerCase() === sayHiToGrandma){
+    return "I can't hear you!"}
+>>>>>>> 3c0621584bf7182d07d4dbb421e3aff69ade8c51
 }
